2a7818a60bd7568ea81dbd43fc937058aff39efb
DkXenos <superjason101010@gmail.com>
2026-05-31 11:03:02 +0700

    added watch view

---
+++ ChinezWatchApp Watch App/ChinezWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChinezWatchAppApp.swift
+//  ChinezWatchApp Watch App
+//
+//  Created by Jason TIo on 31/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ChinezWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
